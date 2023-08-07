3ad350f88dcd903d45fb04c89327193f6f87a4b2
sojin-p <luv2hong@gmail.com>
2023-08-07 12:27:09 +0900

    Git ignore Settiong

--- Week4/ViewController.swift
+++ Week4/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        callReauest()
+    }
+    
+    func callReauest() {
+        //왜 안될까 ? url이 https가 아니고 http임 - info에서 바꾸기 - ATS
+        //인증키 관리 중요!!! -> .gitignore로 중요한 키 숨기기
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
     }
 
 
